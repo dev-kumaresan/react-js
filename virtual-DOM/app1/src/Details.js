28356e18a698c0edce9957f1f16f915bed194fe5
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-05-01 16:58:16 +0530

    updated

---
+++ virtual-DOM/app1/src/Details.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+export function Details() {
+  return (
+  <h1>I am from Pudukkottai - Component2</h1>
+  );
+}
+export default Details;
+
+
+
+
---
+++ virtual-DOM/app1/src/name.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+export function Name() {
+  return (
+    <h1>My name is Kumaresan - component 1</h1>
+  );
+}
+export default Name;
+
+
+
+
