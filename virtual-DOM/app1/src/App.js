29124829a87c42b22aa47eca2f219a2a3d9d927b
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-05-01 17:11:26 +0530

    updated

--- virtual-DOM/app1/src/App.js
+++ virtual-DOM/app1/src/App.js
@@ -4,6 +4,7 @@ import {Details} from  './Details'
 import {Name} from './name'
 export function App() {
   return (
+    
     <div className='root'>
     <div className='name'><Name /></div>
     <div className='details'><Details /></div></div>
