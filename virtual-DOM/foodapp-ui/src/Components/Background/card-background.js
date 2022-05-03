1a157bbabb5ddbeab3c10ca08361ec9735498f80
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-05-03 13:19:08 +0530

    updated

--- virtual-DOM/foodapp-ui/src/Components/Background/card-background.js
+++ virtual-DOM/foodapp-ui/src/Components/Background/card-background.js
@@ -3,11 +3,11 @@ function CardBackground()
 {
 
      return(
-        <div className="container">
+        
         <div className="panel">
-            <h4>Sand</h4>
+            
         </div>
-        </div>
+        
      );
 }
 export default CardBackground;
