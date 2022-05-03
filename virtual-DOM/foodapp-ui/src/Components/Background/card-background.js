1d4fc0aa9e22bbd75c4063f2bef20ea09b6a3ea1
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-05-03 12:51:36 +0530

    updated

---
+++ virtual-DOM/foodapp-ui/src/Components/Background/card-background.js
@@ -0,0 +1,13 @@
+import "./card-bg.css";
+function CardBackground()
+{
+
+     return(
+        <div className="container">
+        <div className="panel">
+            <h4>Sand</h4>
+        </div>
+        </div>
+     );
+}
+export default CardBackground;
