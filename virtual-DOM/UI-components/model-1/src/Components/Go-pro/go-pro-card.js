95947e83cfe6953e8ca525c2855c3125673f73dc
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-05-06 19:58:48 +0530

    updated

--- virtual-DOM/UI-components/model-1/src/Components/Go-pro/go-pro-card.js
+++ virtual-DOM/UI-components/model-1/src/Components/Go-pro/go-pro-card.js
@@ -1,8 +1,7 @@
 import "./go-pro-card.css";
 function GoProCard()
 {
-   return( 
-            <div className="go-pro">
+   return(<div className="go-pro">
             <div className="content">
              <div className="gopro-title">Go Pro</div>
              <div className="gopro-para">Upgrade your plan to get pro benefits</div>
@@ -11,7 +10,6 @@ function GoProCard()
              <div className="button"></div>
              <div className="button-value">Let's Start</div>
             </div>
-            </div>
-   );
+          </div>);
 }
 export default GoProCard;
