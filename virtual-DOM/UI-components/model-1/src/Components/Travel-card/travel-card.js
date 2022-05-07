8a849383b61d956db3580b04e632d7f0eecb77df
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-05-07 10:38:13 +0530

    updated

--- virtual-DOM/UI-components/model-1/src/Components/Travel-card/travel-card.js
+++ virtual-DOM/UI-components/model-1/src/Components/Travel-card/travel-card.js
@@ -29,6 +29,20 @@ function TravelCard()
                             </div>
                         </div>
                     </div>  
+                    <div className="travel-buttons-holder">
+                        <div className="travel-button1-holder">
+                            <div className="button1"></div>
+                            <div className="b1c">Tour</div>
+                        </div>
+                        <div className="travel-button2-holder">
+                            <div className="button2"></div>
+                            <div className="b2c">Trip</div>
+                        </div>
+                        <div className="travel-button3-holder">
+                            <div className="button3"></div>
+                            <div className="b3c">Transport</div>
+                        </div>
+                    </div>
                 </div>
             </div>
     );
