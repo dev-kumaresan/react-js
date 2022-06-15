7d5f6dfcf508d782221d2fc5aa365c97e6c4d85a
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-06-15 14:13:18 +0530

    updated

--- virtual-DOM/UI-components/model-1/src/Components/projects-progress/project-progress.js
+++ virtual-DOM/UI-components/model-1/src/Components/projects-progress/project-progress.js
@@ -3,7 +3,11 @@ import "./project-progress.css";
 function ProjectProgress(){
     return(
         <div className="project-progress-card">
-            
+        <div className="content-holder">
+        <div className="project-progress-content">
+        <div className="project-header">Project Progress</div>
+        </div>
+        </div>
         </div>
     );
 }
